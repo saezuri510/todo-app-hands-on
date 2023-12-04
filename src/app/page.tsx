bed78635ae9e8273365684de82a4438eade256ed
saezuri510 <saezuru510@gmail.com>
2023-12-04 17:21:02 +0900

    feat: prettierをインストール

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 const HomePage = () => {
   return (
     <div>
-      <div className="fixed inset-0 m-auto w-[320px] h-[512px] border-black border">
+      <div className="fixed inset-0 m-auto h-[512px] w-[320px] border border-black">
         <div className="flex items-center justify-center bg-green-400 text-white">
           TODO List
         </div>
